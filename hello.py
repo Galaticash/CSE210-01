6d3ef30b01ba5012a844767797459bcadc7a508e
Galaticash <ash9280@gmail.com>
2022-04-27 11:37:02 -0600

    Adding comments to the hello.py file

--- hello.py
+++ hello.py
@@ -1,1 +1,5 @@
+# successful test with cloning a remote repository from Github,
+# creating new files in the local folder, creating a commit to the local version
+# and then updating the remote repository with files from a local repository
+
 print("Hello World!")
